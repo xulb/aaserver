210758743f327ae82c3949a942a79186798d488b
Xulb Alien <xulb.aa@gmail.com>
2014-12-27 13:24:01 -0500

    load map list to combobox values

--- controller/svr_controller.py
+++ controller/svr_controller.py
@@ -9,6 +9,7 @@ mockServer.current_players = [
 ]
 mockServer.name = 'mock'
 mockServer.admin = 'Xulb'
+mockServer.maplist = "dm8 dm-ahtcity dm-atlantis2k8 dm-babel2k11 dm-blood dm-bloodfactory2k12 dm-bonechewer dm-chasmatic2k9 dm-corrosion dm-crucible2k12 dm-deathray dm-deimos2k9 dm-dismal2k11  dm-dungeon dm-dynamo2k12 dm-egyptian dm-electro dm-eternal  dm-frontier2 dm-furious2k8 dm-gladiator dm-goregrinder dm-hangar-beta dm-horus dm-impact dm-inferno dm-infinic dm-invasion dm-khanate dm-leviathan2k12 dm-liberation dm-mgcity2 dm-module dm-negator dm-neptune dm-nkitrn1 dm-oblivion dm-omega2k8 dm-outpost dm-purgatory  dm-titan2k8 dm-turbo2k8 dm-vesuvius2k11 dm-violator2k11 dm-warmachine2k10 dm-zorn2k11 dm-zion2k9".split()
 
 class ServerController:
     def __init__(self,app):
@@ -17,6 +18,8 @@ class ServerController:
         self.passwd = None
     def _getvar(self,var):
         return self.app.builder.get_variable(var)
+    def _getobj(self,obj):
+        return self.app.builder.get_object(obj,self.app.master)
     def _set_server_and_connect(self, address, passwd):
         self.address = address
         self.passwd = passwd
@@ -26,6 +29,7 @@ class ServerController:
             self.server = mockServer
         self.name = self.server.name
         self.admin = self.server.admin
+        self.maplist = " ".join(self.server.maplist)
 
     def set_server_from_dialog(self):
         addr = tuple(
@@ -34,10 +38,12 @@ class ServerController:
         self._set_server_and_connect(addr,passwd)
         self._getvar('server_name').set(self.name)
         self._getvar('svr_addr').set(":".join(self.address))
+        self._getobj('cmb_maps').configure(values=self.maplist)
         
     def get_current_players(self):
         self.server._poll_status()
         return self.server.current_players
+
 
 #msg_servername
 #msg_svraddr
