6d171f3a154d32675452ece6339604dfb0219654
Xulb Alien <xulb.aa@gmail.com>
2014-12-27 13:24:39 -0500

    add methods for further features (player table)

--- view/app.py
+++ view/app.py
@@ -1,5 +1,6 @@
+#!/usr/bin/python3
 import sys
-sys.path.append('..')
+sys.path.extend(['.','..'])
 from controller.svr_controller import *
 import os
 import tkinter as tk
@@ -17,7 +18,7 @@ class AlienArenaApp(pygubu.TkApplication):
         self.mainwindow = builder.get_object('aaserver_nbk',self.master)
         self.mainmenu = menu = builder.get_object('menu_main',self.master)
         self.set_menu(menu)
-        self.set_title('AA Server')
+        self.set_title('AA Server Control')
         builder.connect_callbacks(self)
 
     def on_menu_item_server_click(self):
@@ -29,6 +30,31 @@ class AlienArenaApp(pygubu.TkApplication):
             self.svr_dialog.toplevel.withdraw()
         except Exception as e:
             msgbox.showerror(title="Connection", message=e)
+
+    def on_send_cmd_click(self):
+        pass
+    def on_start_map_click(self):
+        pass
+
+    def on_refresh_click(self):
+        self._construct_player_table()
+
+    def _construct_player_table(self):
+        players = self.controller.get_current_players()
+        pl_frame = self.builder.get_object('plyr_tab_frame',self.master)
+        lbls_name=[]
+        lbls_addr=[]
+        btns_kick=[]
+        for pl in players:
+            lbls_name.append(tk.Label(pl_frame, width=16, text=pl.stripped_name))
+            lbls_addr.append(tk.Label(pl_frame, width=16, text=pl.address))
+            btns_kick.append(tk.Button(pl_frame, text='Kick'))
+        for (n,a,k) in zip(lbls_name,lbls_addr,btns_kick):
+            n.grid(column=0)
+            ninfo= n.grid_info()
+            a.grid(row=ninfo['row'],column=1)
+            k.grid(row=ninfo['row'],column=2)
+        
 
 if __name__ == '__main__':
     root = tk.Tk()
