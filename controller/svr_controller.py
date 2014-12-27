ae2f3cc72f320c5ede1d4c3b25a29b7d39c1154e
Xulb Alien <xulb.aa@gmail.com>
2014-12-26 21:56:09 -0500

    more features

--- controller/svr_controller.py
+++ controller/svr_controller.py
@@ -1,23 +1,43 @@
 from model.aaserver import *
+from unittest.mock import Mock
+
+mockServer = Mock(spec=AAServer)
+mockServer.current_players = [
+    Player('Xulb','111.222.333.444'),
+    Player('[OS]^1Bis^2ki^7','999.33.2.1'),
+    Player('>>Sander','5.4.3.23')
+]
+mockServer.name = 'mock'
+mockServer.admin = 'Xulb'
 
 class ServerController:
     def __init__(self,app):
         self.app = app
         self.address = None
         self.passwd = None
+    def _getvar(self,var):
+        return self.app.builder.get_variable(var)
     def _set_server_and_connect(self, address, passwd):
         self.address = address
         self.passwd = passwd
-        self.server = AAServer(address, passwd)
+        if passwd!='mock':
+            self.server = AAServer(address, passwd)
+        else :
+            self.server = mockServer
         self.name = self.server.name
         self.admin = self.server.admin
 
     def set_server_from_dialog(self):
         addr = tuple(
-            self.app.builder.get_variable('open_svr_addr').get().split(':'))
-        passwd =self.app.builder.get_variable('open_svr_pass').get()
+            self._getvar('open_svr_addr').get().split(':'))
+        passwd =self._getvar('open_svr_pass').get()
         self._set_server_and_connect(addr,passwd)
-
+        self._getvar('server_name').set(self.name)
+        self._getvar('svr_addr').set(":".join(self.address))
+        
+    def get_current_players(self):
+        self.server._poll_status()
+        return self.server.current_players
 
 #msg_servername
 #msg_svraddr
