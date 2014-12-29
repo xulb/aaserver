02cf84036791790c7b6da4e810b9f4db3fc8df76
Xulb Alien <xulb.aa@gmail.com>
2014-12-28 20:40:08 -0500

    code to set window start position

--- aaserver.py
+++ aaserver.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 import tkinter as tk
 from view.app import *
+
+def _get_geometry(tl_obj):
+    return map(int, re.split('[x+]',tl_obj.geometry()))
+
 root = tk.Tk()
 app = AlienArenaApp(root)
+root.wait_visibility(root)
+(w,h,x,y) = _get_geometry(root)
+root.geometry('%dx%d+%d+%d' % (w,h,(app.ws-w)/2,(app.hs-h)/2))
 app.run()
