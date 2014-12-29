7791ac9a8332f08b6bac3824c7586237d0b3d0b5
Xulb Alien <xulb.aa@gmail.com>
2014-12-28 20:40:42 -0500

    set_dmflags method

--- controller/svr_controller.py
+++ controller/svr_controller.py
@@ -14,6 +14,8 @@ mockServer.name = 'mock'
 mockServer.admin = 'Xulb'
 mockServer.maplist = "dm8 dm-ahtcity dm-atlantis2k8 dm-babel2k11 dm-blood dm-bloodfactory2k12 dm-bonechewer dm-chasmatic2k9 dm-corrosion dm-crucible2k12 dm-deathray dm-deimos2k9 dm-dismal2k11  dm-dungeon dm-dynamo2k12 dm-egyptian dm-electro dm-eternal  dm-frontier2 dm-furious2k8 dm-gladiator dm-goregrinder dm-hangar-beta dm-horus dm-impact dm-inferno dm-infinic dm-invasion dm-khanate dm-leviathan2k12 dm-liberation dm-mgcity2 dm-module dm-negator dm-neptune dm-nkitrn1 dm-oblivion dm-omega2k8 dm-outpost dm-purgatory  dm-titan2k8 dm-turbo2k8 dm-vesuvius2k11 dm-violator2k11 dm-warmachine2k10 dm-zorn2k11 dm-zion2k9".split()
 
+
+
 class ServerController:
     def __init__(self,app):
         self.app = app
@@ -54,6 +56,9 @@ class ServerController:
         
     def get_dmflags(self):
         return self.server.dmflags
+
+    def set_dmflags(self,dmf):
+        self.server.set_dmflags(dmf)
         
 
 #msg_servername
