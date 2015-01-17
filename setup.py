95bb8cbcf2729f870a0a0fbd67203677868b66db
Xulb Alien <xulb.aa@gmail.com>
2015-01-17 09:32:05 -0500

    add setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+setup(
+    name = "aaempire"
+    version = "0.1",
+    packages = find_packages(),
+    scripts = ["aaempire.py"],
+    install_requires = ["pygubu>=0.9.7"],
+    package_data = {
+        '':["*.ui"]
+    },
+    author = "Xulb Alien",
+    author_email = "xulb.aa@gmail.com",
+    description = "A simple GUI for controlling AlienArena servers",
+    license = "PSF",
+    keywords = "alienarena aa quake2 rcon",
+    url = "https://github.com/xulb/aaserver"
+)
